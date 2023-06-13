95519083de852ed547dc064a0f17bc2e4751ec5e
Caio Ramos <aramoscaio@gmail.com>
2023-06-13 19:21:54 -0300

    feat: updated app and requirements

--- app.py
+++ app.py
@@ -3,3 +3,11 @@ from flask import Flask
 def create_app():
 
     app = Flask(__name__)
+
+    db.init_app(app)
+    migrate = Migrate(app, db)
+
+    with app.app_context():
+        db.create_all()
+    
+    return app
