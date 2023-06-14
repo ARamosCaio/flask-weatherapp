d5a9b7008e71325abed91a6fb565d861d83d9996
Caio Ramos <aramoscaio@gmail.com>
2023-06-13 21:55:31 -0300

    feat: updated app and requirements

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask_migrate import Migrate
+from db import db
 
 def create_app():
 
     app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 
     db.init_app(app)
     migrate = Migrate(app, db)
