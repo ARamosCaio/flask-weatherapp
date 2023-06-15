8e5a015d27b099566dfb17e76b9133fef0ccb55d
Caio Ramos <aramoscaio@gmail.com>
2023-06-14 21:44:43 -0300

    fix: moved models to folder models

--- models.py
+++
@@ -1,7 +0,0 @@
-from db import db
-
-class CityModel(db.Model):
-    __tablename__ = "cities"
-
-    id = db.column(db.Integer, primary_key=True)
-    name = db.column(db.String(25), unique=True, nullable=False)
